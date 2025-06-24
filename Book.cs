e053391ccf1858f22861994ab6a38cbebb82f8cf
mhair <mhair@MHAIRI>
2025-06-24 15:35:30 +0100

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace LibraryManagement
+{
+    public class Book
+    {      
+        public string Title { get; set; }
+        public string Author { get; set; }        
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace LibraryManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanelBook = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelBook
+            // 
+            this.flowLayoutPanelBook.Location = new System.Drawing.Point(12, 182);
+            this.flowLayoutPanelBook.Name = "flowLayoutPanelBook";
+            this.flowLayoutPanelBook.Size = new System.Drawing.Size(487, 405);
+            this.flowLayoutPanelBook.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 599);
+            this.Controls.Add(this.flowLayoutPanelBook);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelBook;
+    }
+}
+
